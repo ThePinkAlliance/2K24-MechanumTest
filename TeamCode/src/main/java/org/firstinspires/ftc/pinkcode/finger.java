020b3f9fd324471636aa4c52cd4d63473f025702
LeEpicScoop <wwtipil@gmail.com>
2023-10-26 19:34:13 -0400

    improved finger

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/pinkcode/finger.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.pinkcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous
+public class finger extends OpMode{
+    DcMotor frontRight, frontLeft, backRight, backLeft;
+    int distanceTraveled = 0;
+    @Override
+    public void init() {
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontR");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontL");
+
+        backRight = hardwareMap.get(DcMotorEx.class, "backR");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backL");
+    }
+
+    @Override
+    public void loop() {
+        distanceTraveled = distanceTraveled + 1;
+        telemetry.addData("distance",distanceTraveled);
+        telemetry.addData("frontRight",frontRight.getCurrentPosition());
+        telemetry.addData("frontLeft",frontLeft.getCurrentPosition());
+        telemetry.update();
+        if (Math.abs(frontRight.getCurrentPosition())>500) {
+            this.frontLeft.setPower(0);
+            this.frontRight.setPower(0);
+            this.backLeft.setPower(0);
+            this.backRight.setPower(0);
+
+        }
+        else {
+            this.frontLeft.setPower(1);
+            this.frontRight.setPower(0);
+            this.backLeft.setPower(0);
+            this.backRight.setPower(0);
+        }
+
+    }
+    public void end(){}
+}
